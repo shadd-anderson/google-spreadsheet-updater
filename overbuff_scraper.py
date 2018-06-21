f39be79e60ddc944b484a8036ca10348bdaaa550
Shadd Anderson <shaddanderson18@gmail.com>
2018-06-21 14:33:05 -0500

    refactored some methods into overbuff specific

--- overbuff_scraper.py
+++ overbuff_scraper.py
@@ -1,7 +1,15 @@
+import os
+import time
 import urllib.request
+from datetime import datetime
+from urllib.error import HTTPError
 
 from bs4 import BeautifulSoup
 from urllib.parse import quote
+
+from scraper import update_cell, grab_column
+
+SPREADSHEET_ID = os.environ.get('SHEET_ID')
 
 
 class OverbuffScraper:
@@ -16,3 +24,61 @@ class OverbuffScraper:
         page_contents = BeautifulSoup(page, 'html.parser')
         sr_span = page_contents.find('span', attrs={'class': 'player-skill-rating'})
         return sr_span.text.strip()
+
+    @staticmethod
+    def update_srs(playertag, row):
+        # Tries to get the sr from Overbuff and logs the SR.
+        try:
+            player_sr = OverbuffScraper.sr_fetch(playertag)
+            # print("{}'s SR: {}".format(player_tag, player_sr))
+
+        # Catches incorrect battletags
+        except HTTPError:
+            print("Error retrieving {}'s page. Debug/update battletag".format(playertag))
+            body = {
+                'values': [["""Battletag incorrect? (Case-sensitive) - If you see a whole column of these, """
+                            """PM swallama NOW!!"""]]
+            }
+            update_cell(SPREADSHEET_ID, ('Roster!G' + str(row)), body)
+
+        # except UnicodeEncodeError:
+        #     """Handles situations with odd-ball battletags"""
+        #     # print("{}'s SR: {}".format(player_tag.encode('utf-8').strip(), player_sr))
+        #     body = {
+        #         'values': [[player_sr]]
+        #     }
+        #     update_cell(SPREADSHEET_ID, ('Roster!F' + str(row)), body)
+        #     update_cell(SPREADSHEET_ID, ('Roster!G' + str(row)), {'values': [[""]]})
+
+        # Handles a un-reported/unranked player
+        except AttributeError:
+            print("{} has no SR reported".format(playertag))
+            body = {
+                'values': [[r"**Not current**"]]
+            }
+            update_cell(SPREADSHEET_ID, ('Roster!G' + str(row)), body)
+        # If everything is good to go, updates the spreadsheet
+        else:
+            body = {
+                'values': [[player_sr]]
+            }
+            update_cell(SPREADSHEET_ID, ('Roster!F' + str(row)), body)
+            update_cell(SPREADSHEET_ID, ('Roster!G' + str(row)), {'values': [[""]]})
+
+
+if __name__ == 'main':
+    battletags = grab_column(SPREADSHEET_ID, "D", sheetname="Roster")
+    print("Running scraper on " + datetime.strftime(datetime.now(), "%b %d, %I:%M%p"))
+    for index, battletag in enumerate(battletags, start=1):
+        """Checks to make sure there's something in the cell. If not, moves on to the next one"""
+        try:
+            player_tag = battletag[0]
+        except IndexError:
+            continue
+
+        if player_tag != 'Battletag':
+            """Skips past the column headers"""
+            OverbuffScraper.update_srs(player_tag, index)
+            time.sleep(1)
+    print("Scraper successfully completed at " + datetime.strftime(datetime.now(), "%I:%M%p"))
+    print("Please see above for errors")
--- scraper.py
+++ scraper.py
@@ -1,14 +1,8 @@
-import datetime
 import os
-import time
 
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import client
-from urllib.error import HTTPError
-
-# Sets up the API
-from overbuff_scraper import OverbuffScraper
 
 
 class Scraper:
@@ -18,9 +12,6 @@ class Scraper:
 credential_json = os.environ.get("CREDENTIALS")
 creds = client.OAuth2Credentials.from_json(credential_json)
 service = discovery.build('sheets', 'v4', http=creds.authorize(Http()))
-
-# ID of the spreadsheet
-SPREADSHEET_ID = os.environ.get('SHEET_ID')
 
 # print(OverbuffScraper.sr_fetch("swallama#1813"))
 # ^ For debugging ^
@@ -39,64 +30,6 @@ def grab_column(spreadsheetid, column, sheetname=""):
     return column_values.get('values')
 
 
-def update_sr(playertag, row):
-    # Tries to get the sr from Overbuff and logs the SR.
-    try:
-        player_sr = OverbuffScraper.sr_fetch(playertag)
-        # print("{}'s SR: {}".format(player_tag, player_sr))
-
-    # Catches incorrect battletags
-    except HTTPError:
-        print("Error retrieving {}'s page. Debug/update battletag".format(playertag))
-        body = {
-            'values': [["""Battletag incorrect? (Case-sensitive) - If you see a whole column of these, """
-                        """PM swallama NOW!!"""]]
-        }
-        service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=('Roster!G' + str(row)),
-                                               valueInputOption='USER_ENTERED', body=body).execute()
-    # except UnicodeEncodeError:
-    #     """Handles situations with odd-ball battletags"""
-    #     # print("{}'s SR: {}".format(player_tag.encode('utf-8').strip(), player_sr))
-    #     body = {
-    #         'values': [[player_sr]]
-    #     }
-    #     service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=('Roster!F' + str(row)),
-    #                                            valueInputOption='USER_ENTERED', body=body).execute()
-    #     service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=('Roster!G' + str(row)),
-    #                                            valueInputOption='USER_ENTERED', body={'values': [[""]]}).execute()
-
-    # Handles a un-reported/unranked player
-    except AttributeError:
-        print("{} has no SR reported".format(playertag))
-        body = {
-            'values': [[r"**Not current**"]]
-        }
-        service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=('Roster!G' + str(row)),
-                                               valueInputOption='USER_ENTERED', body=body).execute()
-    # If everything is good to go, updates the spreadsheet
-    else:
-        body = {
-            'values': [[player_sr]]
-        }
-        service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=('Roster!F' + str(row)),
-                                               valueInputOption='USER_ENTERED', body=body).execute()
-        service.spreadsheets().values().update(spreadsheetId=SPREADSHEET_ID, range=('Roster!G' + str(row)),
-                                               valueInputOption='USER_ENTERED', body={'values': [[""]]}).execute()
-
-
-# Grabs all the values in the "Battletag" column of the spreadsheet
-battletags = grab_column(SPREADSHEET_ID, "D", sheetname="Roster")
-print("Running scraper on " + datetime.datetime.strftime(datetime.datetime.now(), "%b %d, %I:%M%p"))
-for index, battletag in enumerate(battletags, start=1):
-    """Checks to make sure there's something in the cell. If not, moves on to the next one"""
-    try:
-        player_tag = battletag[0]
-    except IndexError:
-        continue
-
-    if player_tag != 'Battletag':
-        """Skips past the column headers"""
-        update_sr(player_tag, index)
-        time.sleep(1)
-print("Scraper successfully completed at " + datetime.datetime.strftime(datetime.datetime.now(), "%I:%M%p"))
-print("Please see above for errors")
+def update_cell(spreadsheetid, cell, body):
+    service.spreadsheets().values().update(spreadsheetId=spreadsheetid, range=cell,
+                                           valueInputOption='USER_ENTERED', body=body).execute()
