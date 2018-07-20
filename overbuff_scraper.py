cc846e021d384b378b134f873409a282cfb747ec
Shadd Anderson <shaddanderson18@gmail.com>
2018-07-19 22:54:38 -0500

    adding career high column

--- overbuff_scraper.py
+++ overbuff_scraper.py
@@ -9,6 +9,11 @@ from urllib.parse import quote
 
 from spreadsheet_update import update_cell, grab_column
 
+sr_column = 'Roster!F'
+career_high_column = 'Roster!G'
+last_updated_column = 'Roster!H'
+notes_column = 'Roster!I'
+
 SPREADSHEET_ID = os.environ.get('SHEET_ID')
 
 
@@ -29,7 +34,7 @@ class OverbuffScraper:
         return sr_span.text.strip(), ispublic
 
     @staticmethod
-    def update_srs(playertag, row, date):
+    def update_srs(playertag, row, date, career_high):
         """Tries to get the sr from Overbuff and logs the SR."""
         try:
             player_sr, ispublic = OverbuffScraper.sr_fetch(playertag)
@@ -42,7 +47,7 @@ class OverbuffScraper:
                 'values': [["""Battletag incorrect? (Case-sensitive) - If you see a whole column of these, """
                             """PM swallama NOW!!"""]]
             }
-            update_cell(SPREADSHEET_ID, ('Roster!H' + str(row)), body)
+            update_cell(SPREADSHEET_ID, (notes_column + str(row)), body)
 
         # except UnicodeEncodeError:
         #     """Handles situations with odd-ball battletags"""
@@ -59,24 +64,27 @@ class OverbuffScraper:
             body = {
                 'values': [[r"**Not current**"]]
             }
-            update_cell(SPREADSHEET_ID, ('Roster!H' + str(row)), body)
+            update_cell(SPREADSHEET_ID, (notes_column + str(row)), body)
         # If everything is good to go, updates the spreadsheet
         else:
             body = {
                 'values': [[player_sr]]
             }
-            update_cell(SPREADSHEET_ID, ('Roster!F' + str(row)), body)
+            update_cell(SPREADSHEET_ID, (sr_column + str(row)), body)
             if ispublic:
-                update_cell(SPREADSHEET_ID, ('Roster!H' + str(row)), {'values': [[""]]})
-                update_cell(SPREADSHEET_ID, ('Roster!G' + str(row)), {'values': [[date]]})
+                update_cell(SPREADSHEET_ID, (notes_column + str(row)), {'values': [[""]]})
+                update_cell(SPREADSHEET_ID, (last_updated_column + str(row)), {'values': [[date]]})
+                if len(career_high) == 0 or player_sr > career_high[0]:
+                    update_cell(SPREADSHEET_ID, (career_high_column + str(row)), {'values': [[player_sr]]})
             else:
-                update_cell(SPREADSHEET_ID, ('Roster!H' + str(row)), {'values': [["Private profile"]]})
+                update_cell(SPREADSHEET_ID, (notes_column + str(row)), {'values': [["Private profile"]]})
                 print("{} has a private profile!".format(playertag))
 
 
 if __name__ == '__main__':
     current_time = datetime.now()
     battletags = grab_column(SPREADSHEET_ID, "D", sheetname="Roster")
+    career_highs = grab_column(SPREADSHEET_ID, "G", sheetname="Roster")
     print("Running scraper on " + datetime.strftime(current_time, "%b %d, %I:%M%p"))
     for index, battletag in enumerate(battletags, start=1):
         """Checks to make sure there's something in the cell. If not, moves on to the next one"""
@@ -87,8 +95,14 @@ if __name__ == '__main__':
 
         if player_tag != 'Battletag':
             """Skips past the column headers"""
-            OverbuffScraper.update_srs(player_tag, index, datetime.strftime(current_time, "%d/%m/%y"))
+            OverbuffScraper.update_srs(playertag=player_tag,
+                                       row=index,
+                                       date=datetime.strftime(current_time, "%d/%m/%y"),
+                                       career_high=career_highs[index])
             time.sleep(2)
     print("Scraper successfully completed at " + datetime.strftime(datetime.now(), "%I:%M%p"))
     print("Please see above for errors")
-    # OverbuffScraper.update_srs("SolusPrime42#1304", 188, datetime.strftime(current_time, "%d/%m/%y"))
+    # OverbuffScraper.update_srs("SolusPrime42#1304",
+    #                            188,
+    #                            datetime.strftime(current_time, "%d/%m/%y"),
+    #                            career_highs[188])
